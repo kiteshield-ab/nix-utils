cfd529cd23d8cd160333c6be1c6591b41f80330f
Gabriel Górski <gabriel.gorski@kiteshield.com>
2024-11-05 22:51:36 +0100

    Add libsigrok overlay with extra fw

--- flake.nix
+++ flake.nix
@@ -6,14 +6,23 @@
     inputs@{ self, ... }:
     let
       system = "x86_64-linux";
-      nixpkgs = import inputs.nixpkgs { inherit system; };
+      nixpkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          self.overlays.libsigrok-dreamsourcelab-fw
+        ];
+      };
       inherit (nixpkgs) pkgs;
     in
     {
       formatter.${system} = pkgs.nixfmt-rfc-style;
       packages.${system} = {
+        sigrok-cli = pkgs.sigrok-cli;
         linkserver = pkgs.callPackage ./packages/linkserver.nix { };
         mcuxpresso-config-tools = pkgs.callPackage ./packages/mcuxpresso-config-tools.nix { };
+      };
+      overlays = {
+        libsigrok-dreamsourcelab-fw = import ./overlays/libsigrok-dreamsourcelab-fw.nix;
       };
     };
 }
