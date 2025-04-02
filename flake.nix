731ee1755cf4267fddea0446130b72e1dbf9666b
Emil Fresk <emil.fresk@kiteshield.com>
2025-04-02 13:50:46 +0200

    aarch64-linux support

--- flake.nix
+++ flake.nix
@@ -3,30 +3,45 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
-    inputs@{ self, ... }:
+    inputs@{ self, nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      nixpkgs = import inputs.nixpkgs {
+      # forAllSystems follows this guide:
+      # https://github.com/Misterio77/nix-starter-configs/issues/64#issuecomment-1941420712
+      pkgsFor = system: {
         inherit system;
-        overlays = [
-          self.overlays.libsigrok-dreamsourcelab-fw
-        ];
+
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlays.libsigrok-dreamsourcelab-fw
+          ];
+        };
       };
-      inherit (nixpkgs) pkgs;
+
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (pkgsFor system));
     in
     {
-      formatter.${system} = pkgs.nixfmt-rfc-style;
-      packages.${system} = with pkgs; {
-        # Embedded development miscellaneous applications
-        arm-compiler = callPackage ./packages/arm-compiler.nix { };
-        linkserver = callPackage ./packages/linkserver.nix { };
-        mcuxpresso-config-tools = callPackage ./packages/mcuxpresso-config-tools.nix { };
-        mcuxpresso-ide = callPackage ./packages/mcuxpresso-ide.nix { };
-        # Rust utilities
-        chiptool = callPackage ./packages/chiptool.nix { };
-        form = callPackage ./packages/form.nix { };
-        mdbook-wavedrom = callPackage ./packages/mdbook-wavedrom.nix { };
-      };
+      formatter = forAllSystems ({ pkgs, ... }: pkgs.nixfmt-rfc-style);
+      packages = forAllSystems (
+        { pkgs, ... }:
+        with pkgs;
+        {
+          # Embedded development miscellaneous applications
+          arm-compiler = callPackage ./packages/arm-compiler.nix { };
+          linkserver = callPackage ./packages/linkserver.nix { };
+          mcuxpresso-config-tools = callPackage ./packages/mcuxpresso-config-tools.nix { };
+          mcuxpresso-ide = callPackage ./packages/mcuxpresso-ide.nix { };
+          # Rust utilities
+          chiptool = callPackage ./packages/chiptool.nix { };
+          form = callPackage ./packages/form.nix { };
+          mdbook-wavedrom = callPackage ./packages/mdbook-wavedrom.nix { };
+        }
+      );
       overlays = {
         # Overlay for libsigrok library containing extra firmware
         libsigrok-dreamsourcelab-fw = import ./overlays/libsigrok-dreamsourcelab-fw.nix;
